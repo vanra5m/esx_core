cbec814bb6071cdede8e4e90434494b87fb36312
Vanra <vanra5m@proton.me>
2025-03-01 02:17:39 -0500

    Update main.lua
    No need to redeclare source.

--- [core]/esx_multicharacter/server/main.lua
+++ [core]/esx_multicharacter/server/main.lua
@@ -7,17 +7,14 @@ Server.prefix = Config.Prefix or "char"
 Server.identifierType = ESX.GetConfig().Identifier or GetConvar("sv_lan", "") == "true" and "ip" or "license"
 
 AddEventHandler("playerConnecting", function(_, _, deferrals)
-   local source = source
    Server:OnConnecting(source, deferrals)
 end)
 
 RegisterNetEvent("esx_multicharacter:SetupCharacters", function()
-    local source = source
     Multicharacter:SetupCharacters(source)
 end)
 
 RegisterNetEvent("esx_multicharacter:CharacterChosen", function(charid, isNew)
-    local source = source
     Multicharacter:CharacterChosen(source, charid, isNew)
 end)
 
@@ -26,7 +23,6 @@ AddEventHandler("esx_identity:completedRegistration", function(source, data)
 end)
 
 AddEventHandler("playerDropped", function()
-    local source = source
     Multicharacter:PlayerDropped(source)
 end)
 
@@ -34,11 +30,9 @@ RegisterNetEvent("esx_multicharacter:DeleteCharacter", function(charid)
     if not Config.CanDelete or type(charid) ~= "number" or string.len(charid) > 2 then
         return
     end
-    local source = source
     Database:DeleteCharacter(source, charid)
 end)
 
 RegisterNetEvent("esx_multicharacter:relog", function()
-    local source = source
     TriggerEvent("esx:playerLogout", source)
 end)
